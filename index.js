b9b22304c85dca61c55c0520484d1c80ed2abcb8
JesseKay <kayjes00@gmail.com>
2024-05-28 22:21:20 +0200

    make links interact

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const hamMenu = document.querySelector('.ham-menu');
 
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const menuLinks = offScreenMenu.querySelectorAll("li");
+// const mediaLinks = mediaLinks.querySelector('.media');
+
+
     hamMenu.addEventListener('click', () => {
       hamMenu.classList.toggle('active');
       offScreenMenu.classList.toggle('active');
+    });
+
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        hamMenu.classList.remove('active');
+        offScreenMenu.classList.remove('active');
+        // mediaLinks.classList.remove('active');
+      });
     });
